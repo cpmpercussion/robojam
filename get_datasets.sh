515159bcecd0c7d6650da71a2bbdad56c2c9003e
Charles Martin <cpm@charlesmartin.com.au>
2018-11-22 14:10:19 +0100

    added updated version of metatone dataset

--- get_datasets.sh
+++ get_datasets.sh
@@ -5,4 +5,5 @@ cd datasets
 #wget http://folk.uio.no/charlepm/datasets/TinyPerformanceCorpus.h5
 #wget http://folk.uio.no/charlepm/datasets/MetatoneTinyPerformanceRecords.h5
 wget http://folk.uio.no/charlepm/datasets/tiny_performance_datasets.npz
+wget http://folk.uio.no/charlepm/datasets/metatone_dataset.npz
 cd ..
