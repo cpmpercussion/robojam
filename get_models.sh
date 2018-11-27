61169c8fb8ef8a80fab08791c6943a3bf31b932a
Charles Martin <cpm@charlesmartin.com.au>
2018-11-27 11:01:48 +0100

    updated model downloader

--- get_models.sh
+++ get_models.sh
@@ -17,6 +17,7 @@ cd models
 # wget http://folk.uio.no/charlepm/robojam_models/mdrnn-2d-1d-3layers-64units-16mixtures.index
 # wget http://folk.uio.no/charlepm/robojam_models/mdrnn-2d-1d-3layers-64units-16mixtures.meta
 # wget http://folk.uio.no/charlepm/robojam_models/robojam-mdn-rnn.h5
-wget http://folk.uio.no/charlepm/robojam_models/robojam-td-model-E12-VL-4.57.hdf5
+#wget http://folk.uio.no/charlepm/robojam_models/robojam-td-model-E12-VL-4.57.hdf5
+wget http://folk.uio.no/charlepm/robojam_models/robojam-metatone-layers2-units512-mixtures5-scale10-E30-VL-5.65.hdf5
 
 cd ..
