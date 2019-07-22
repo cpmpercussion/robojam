f6e2b821bac78b65d1c09b2dfd6ca783d692b452
Charles Martin <cpm@charlesmartin.com.au>
2019-07-22 20:53:24 +1000

    removed plotting functions from main robojam library to avoid matplotlib

--- robojam/__init__.py
+++ robojam/__init__.py
@@ -10,7 +10,6 @@ import keras
 from .sample_data import *
 # from .tiny_performance_player import *
 # from .tiny_performance_loader import *
-import matplotlib.pyplot as plt
 
 
 def build_robojam_model(seq_len=30, hidden_units=256, num_mixtures=5, layers=2, time_dist=True, inference=False, compile_model=True, print_summary=True, predict_moving=False):
@@ -188,47 +187,3 @@ def divide_performance_into_swipes(perf_df):
         remainder = remainder.iloc[remainder.index >= att]
     performance_swipes.append(remainder)
     return performance_swipes
-
-
-input_colour = 'darkblue'
-gen_colour = 'firebrick'
-
-
-def plot_2D(perf_df, name="foo", saving=False, figsize=(8, 8)):
-    """Plot a 2D representation of a performance 2D"""
-    swipes = divide_performance_into_swipes(perf_df)
-    plt.figure(figsize=figsize)
-    for swipe in swipes:
-        p = plt.plot(swipe.x, swipe.y, 'o-')
-        plt.setp(p, color=gen_colour, linewidth=5.0)
-    plt.ylim(1.0,0)
-    plt.xlim(0,1.0)
-    plt.xticks([])
-    plt.yticks([])
-    if saving:
-        plt.savefig(name+".png", bbox_inches='tight')
-        plt.close()
-    else:
-        plt.show()
-
-
-def plot_double_2d(perf1, perf2, name="foo", saving=False, figsize=(8, 8)):
-    """Plot two performances in 2D"""
-    plt.figure(figsize=figsize)
-    swipes = divide_performance_into_swipes(perf1)
-    for swipe in swipes:
-        p = plt.plot(swipe.x, swipe.y, 'o-')
-        plt.setp(p, color=input_colour, linewidth=5.0)
-    swipes = divide_performance_into_swipes(perf2)
-    for swipe in swipes:
-        p = plt.plot(swipe.x, swipe.y, 'o-')
-        plt.setp(p, color=gen_colour, linewidth=5.0)
-    plt.ylim(1.0,0)
-    plt.xlim(0,1.0)
-    plt.xticks([])
-    plt.yticks([])
-    if saving:
-        plt.savefig(name+".png", bbox_inches='tight')
-        plt.close()
-    else:
-        plt.show()
---
+++ robojam/performance_plotters.py
@@ -0,0 +1,47 @@
+"""Functions to Plot RoboJam Performances
+"""
+import matplotlib.pyplot as plt
+
+
+input_colour = 'darkblue'
+gen_colour = 'firebrick'
+
+
+def plot_2D(perf_df, name="foo", saving=False, figsize=(8, 8)):
+    """Plot a 2D representation of a performance 2D"""
+    swipes = divide_performance_into_swipes(perf_df)
+    plt.figure(figsize=figsize)
+    for swipe in swipes:
+        p = plt.plot(swipe.x, swipe.y, 'o-')
+        plt.setp(p, color=gen_colour, linewidth=5.0)
+    plt.ylim(1.0,0)
+    plt.xlim(0,1.0)
+    plt.xticks([])
+    plt.yticks([])
+    if saving:
+        plt.savefig(name+".png", bbox_inches='tight')
+        plt.close()
+    else:
+        plt.show()
+
+
+def plot_double_2d(perf1, perf2, name="foo", saving=False, figsize=(8, 8)):
+    """Plot two performances in 2D"""
+    plt.figure(figsize=figsize)
+    swipes = divide_performance_into_swipes(perf1)
+    for swipe in swipes:
+        p = plt.plot(swipe.x, swipe.y, 'o-')
+        plt.setp(p, color=input_colour, linewidth=5.0)
+    swipes = divide_performance_into_swipes(perf2)
+    for swipe in swipes:
+        p = plt.plot(swipe.x, swipe.y, 'o-')
+        plt.setp(p, color=gen_colour, linewidth=5.0)
+    plt.ylim(1.0,0)
+    plt.xlim(0,1.0)
+    plt.xticks([])
+    plt.yticks([])
+    if saving:
+        plt.savefig(name+".png", bbox_inches='tight')
+        plt.close()
+    else:
+        plt.show()
